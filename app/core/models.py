7cd8a953ecdb5b25ff991a631617c7d1c75deb8b
anuppathak <anup@keywordio.com>
2020-01-01 14:42:30 +0530

    added working list tags in app

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
 
 class UserManager(BaseUserManager):
 
